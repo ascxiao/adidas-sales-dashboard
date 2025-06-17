d240f9c133905da2cab6c5122c08d37e06328b75
Carlos Valderrama <81756261+ascxiao@users.noreply.github.com>
2025-06-17 22:41:50 +0800

    Added more stuff

--- app.py
+++ app.py
@@ -42,11 +42,65 @@ with col4:
     st.plotly_chart(fig, use_container_width=True)
 
 _, view1, dwn1, view2, dwn2 = st.columns([0.15, 0.20, 0.20, 0.20, 0.20])
+
 with view1:
     expander = st.expander("Sales by Retailer Data")
-    data = df[["Retailer", "TotalSales"]].groupby(by="Retailer")["TotalSales"].sum().sort_values(ascending = False)
+    data = df[["Retailer", "TotalSales"]].groupby("Retailer")["TotalSales"].sum().sort_values(ascending = False)
     expander.write(data)
 
 with dwn1:
     st.download_button("Download Sales by Retailer", data = data.to_csv().encode("utf-8"),
                        file_name = "RetailerSales.csv", mime="text/csv")
+    
+
+df["Month_year"] = df['InvoiceDate'].dt.strftime('%b %Y')
+result = df.groupby('Month_year')['TotalSales'].sum().reset_index()
+
+with col5:
+    fig1 = px.line(result, x = 'Month_year', y = 'TotalSales', title = 'Title Sales Over Time', template = 'gridon')
+    st.plotly_chart(fig1, use_container_width = True)
+
+with view2:
+    expander = st.expander("Monthly Sales")
+    data = result
+    expander.write(data)
+
+with dwn2:
+    st.download_button("Download Monthly Sales", data = data.to_csv().encode("utf-8"),
+                       file_name = "MonthlySales.csv", mime="text/csv")
+st.divider()
+
+result1 = df.groupby('State')[['TotalSales', 'UnitsSold']].sum().reset_index()
+
+#Add the Units Sold as a line chart on a secondary y - axis
+
+fig3 = go.Figure()
+fig3.add_trace(go.Bar(x=result1["State"], y=result1["TotalSales"], name = "Total Sales"))
+fig3.add_trace(go.Scatter(x=result1["State"], y=result1["UnitsSold"], mode = "lines", name = "Units Sold", yaxis='y2'))
+fig3.update_layout(
+    title = "Total Sales and Units Sold by State",
+    xaxis = dict(title="State"),
+    yaxis = dict(title = "Total Sales", showgrid = False),
+    yaxis2 = dict(title="Units Sold", overlaying = "y", side = "right"),
+    template = "gridon",
+    legend = dict(x=1, y=1.1)
+)
+
+_, col6 = st.columns([0.1, 1])
+with col6:
+    st.plotly_chart(fig3, use_container_width= True)
+
+_, view3, dwn3 = st.columns([0.5, 0.45, 0.45])
+
+with view3:
+    expander = st.expander("View Data Sales by Units Sold")
+    expander.write(result1)
+with dwn3:
+    st.download_button("Get Data", data=result.to_csv().encode("utf-8"),
+                       file_name = "Sales_by_UnitsSold.csv", mime="text/csv")
+st.divider()
+
+_, col7 = st.columns =([0.1, 1])
+treemap = df[['Region', 'City', 'TotalSales']].hroupby(by = ['Region', 'City'])['TotalSales']
+
+fig4 = px.treemap(treemap, path = ['Region',])
